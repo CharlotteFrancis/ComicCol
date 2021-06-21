8af0365caa77ae9904f9c8fd174b1bff3b261a16
Deviant <michaelngo1337@yahoo.com>
2021-06-21 11:36:41 -0700

    implemented model index.js file

--- models/index.js
+++ models/index.js
@@ -1,5 +1,43 @@
 const User = require('./User.js')
+const Comic = require('./Comic.js')
+const ComicList = require('./ComicList.js')
+const List = require('./List.js')
+const Comment = require('./Comment.js')
 
 // your relationships go here...
 
-module.exports = { User }
+// a user has many comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+})
+
+// a comment belongs to a single user
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+// a user has one list
+User.hasOne(List, {
+  foreignKey: 'user_id'
+})
+
+// a list belongs to one user
+List.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+// a comic belongs to many lists
+Comic.belongsToMany(List, {
+  through: ComicList,
+  foreignKey: 'comic_id',
+  otherKey: 'list_id'
+})
+
+// a list has many comics, we use belongsToMany here because of the many to many association
+List.belongsToMany(Comic, {
+  through: ComicList,
+  foreignKey: 'list_id',
+  otherKey: 'comic_id'
+})
+
+module.exports = { User, Comic, ComicList, List, Comment }
