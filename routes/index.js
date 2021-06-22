866c471877732448d974182f4fd553e3d134fb41
Bronson Perez <bperez1081@gmail.com>
2021-06-22 10:54:41 -0700

    more routes california is in a drought

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
 router.use('/api', require('./userRoutes.js'))
+router.use('/api', require('./commentRoutes.js'))
+router.use('/api', require('./comicRoutes.js'))
 // other routers go here...
 
 module.exports = router
