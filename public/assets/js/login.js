9dbbc039fae9aeb0e3a270c84d91737ea5a8b45d
Deviant <michaelngo1337@yahoo.com>
2021-06-22 10:36:47 -0700

    implemented login feature in login.js

---
+++ public/assets/js/login.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+
+document.getElementById('submitLogIn').addEventListener('click', event => {
+  event.preventDefault()
+  axios.post('/api/users/login', {
+    username: document.getElementById('username').value,
+    password: document.getElementById('password').value
+  })
+  .then(({data: token}) => {
+    if (token) {
+      localStorage.setItem('token', token)
+      window.location = '/'
+    }
+    else {
+      alert('Invalid username or password')
+    }
+  })
+  .catch(err => console.error(err))
+})
