37b5cc39df1496d72050cd682cd4832231349358
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-23 02:43:19 -0700

    took out console log on comicvine query

--- routes/comicVineRoutes.js
+++ routes/comicVineRoutes.js
@@ -3,8 +3,6 @@ const axios = require('axios')
 require('dotenv').config()
 
 router.get('/comicVine/:query', (req, res) => {
-  console.log(req.params.query)
-  console.log('hello world')
   axios({
     method: 'get',
     url: `https://comicvine.gamespot.com/api/search/?api_key=${process.env.API_KEY}&format=json&limit=18&sort=name:asc&resources=issue&query=${req.params.query}`,
