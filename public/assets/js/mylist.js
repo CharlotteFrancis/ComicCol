b990e2739791e0b432e9aba30ee2446050c0df3a
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-23 10:48:42 -0700

    added logic steps in comments plus search button

---
+++ public/assets/js/mylist.js
@@ -0,0 +1,13 @@
+// mylist.js logic
+
+// needs to render the list from user's List.
+// the actual thing we are rendering is the List's "ComicList" array.
+
+// get the comic with a Comic.findOne using the comic id in ComicList.
+// COVER || TITLE || ISSUE NUMBER || ISSUE NAME (if exists)
+
+// get the last 2 pieces of data from ComicList
+// SCORE(rating) || PROGRESS(read_status?)
+
+// UPDATE button should only work with the ComicList object.
+// put a data-id attriute on the button when rendering
