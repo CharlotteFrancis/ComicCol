30fc30c5677c9e4bba2961f8d3c52514a087f4ad
Deviant <michaelngo1337@yahoo.com>
2021-06-23 11:55:49 -0700

    added routes to comicRoutes, changed comic model, implemented myList.js

--- routes/comicRoutes.js
+++ routes/comicRoutes.js
@@ -1,16 +1,23 @@
 const router = require('express').Router()
-const { Comic } = require('../models')
+const { Comic, List, ComicList } = require('../models')
 const passport = require('passport')
 
+// gets the current user's list of comics
 router.get('/comic', passport.authenticate('jwt'), (req, res) => {
-  res.json(req.user.comic)
+  res.json(req.user.list)
 })
 
-router.post('/comic', passport.authenticate('jwt'), (req, res) => Comic.create({
-  text: req.body.text,
-  isDone: req.body.isDone,
-  uid: req.user.id
+// gets a comic by its image url
+router.get('/comic/:url', passport.authenticate('jwt'), (req, res) => {
+  Comic.findOne({
+    where: { cover_image: req.params.url }
+  })
+  .then(comic => res.json(comic))
+  .catch(err => res.status(400).json(err))
 })
+
+// creates a new comic
+router.post('/comic', passport.authenticate('jwt'), (req, res) => Comic.create(req.body)
   .then(comic => res.json(comic))
   .catch(err => console.log(err)))
 
