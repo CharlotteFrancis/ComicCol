51f4e1e39e954096110349a5fa060a4a2f9b21e8
Deviant <michaelngo1337@yahoo.com>
2021-06-23 14:31:40 -0700

    changed routes and updated comic model

--- routes/comicRoutes.js
+++ routes/comicRoutes.js
@@ -7,8 +7,12 @@ router.get('/comic', passport.authenticate('jwt'), (req, res) => {
 })
 
 router.post('/comic', passport.authenticate('jwt'), (req, res) => Comic.create({
-  text: req.body.text,
-  isDone: req.body.isDone,
+  name: req.body.name,
+  description: req.body.description,
+  cover_image: req.body.cover_image,
+  issue_number: req.body.issue_number,
+  issue_name: req.body.issue_name,
+  cover_date: req.body.cover_date,
   uid: req.user.id
 })
   .then(comic => res.json(comic))
