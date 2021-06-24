680fd41c8f9ca04e7c8f89db9b9e7a680b967754
Charlotte Wong <ChiharuTaru@gmail.com>
2021-06-24 01:38:31 -0700

    Carousel mobile responsive + list format

--- routes/comicListRoutes.js
+++ routes/comicListRoutes.js
@@ -5,10 +5,10 @@ const passport = require('passport')
 // finds all comic lists
 router.get('/comiclist', passport.authenticate('jwt'), (req, res) => {
   ComicList.findAll({
-    include: [ {model: List}, {model: Comic} ]
+    include: [{ model: List }, { model: Comic }]
   })
-  .then(comicLists => res.json(comicLists))
-  .catch(err => console.log(err))
+    .then(comicLists => res.json(comicLists))
+    .catch(err => console.log(err))
 })
 
 router.post('/comiclist', passport.authenticate('jwt'), (req, res) => ComicList.create({
