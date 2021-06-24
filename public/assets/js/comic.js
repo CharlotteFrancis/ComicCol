c08ca4e414c01ba3509ae469bda90302438ae337
Jacob Zavita <jacobzavita@gmail.com>
2021-06-23 20:25:11 -0700

    rendering on comic.html

--- public/assets/js/comic.js
+++ public/assets/js/comic.js
@@ -1,14 +1,3 @@
-// STARTING OVER
-// LOGIC
-// needs to pull from comicvine based on the comic icon they click on either the search results or mylist.
-
-// How to get a specific issue when they click on it -- use the data from the previous page
-
-// const clearData = _ => {
-//   document.getElementById('comicData').innerHTML = ''
-//   document.getElementById('comicCover').innerHTML = ''
-// }
-
 // Query url stuff
 const params = new URLSearchParams(window.location.search)
 const query = params.get('query1')
@@ -16,30 +5,29 @@ const getComic = _ => {
   axios.get(`/api/comicVine/comicPage/${query}`)
     .then((comic) => {
       // COMIC DATA TO RENDER IS STORED IN comic.data
-      console.log(comic.data)
+      console.log(comic.data[0])
+      renderSingleComic(comic)
     })
     .catch(err => console.log(err))
 }
 
 getComic()
-//
 
-renderComicTest = () => {
+renderSingleComic = (comic) => {
   let comicData = document.createElement('div')
   comicData.classList = 'card horizontal rounded-corners'
   comicData.innerHTML = `
       <div class="card-image">
-        <img class="materialboxed" src="https://comicvine.gamespot.com/a/uploads/scale_large/6/67663/6686014-07.jpg">
+        <img class="materialboxed" src="${comic.data[0].image.super_url}">
       </div>
       <div class="card-stacked">
         <div class="card-content">
-          <h2>Thor #7</h2>
-          <h3>Young Thor's Lament</h3>
-          <h4>Cover Date: 2019-01-01</h4>
+          <h2>${comic.data[0].volume.name} #${comic.data[0].issue_number}</h2>
+          <h3>${comic.data[0].name}</h3>
+          <h4>Cover Date: ${comic.data[0].cover_date}</h4>
           <h4>Average Rating: X/10</h4>
           <h5>Description</h5>
-          <p><em>A TALE OF YOUNG THOR FROM THE VIKING AGE!</em></p>
-              <p><em>Every time Thor comes to Midgard, he gets all the mead, battle and romance a young god could possibly want. But he still can't figure out how to prove himself worthy of Mjolnir. And now Odin is determined to keep his son away from Midgard for good. And young Loki knows just the way to do it.</em></p>
+          ${comic.data[0].description}
         </div>
         <div class="card-action">
         <p class="right">Add to your collection</p>
@@ -49,39 +37,3 @@ renderComicTest = () => {
     `
   document.getElementById('comicCard').append(comicData)
 }
-
-renderComicTest()
-
-renderComicTest2 = () => {
-  axios.get(`/api/comicVine/${query}`)
-    .then((comic) => {
-      renderComicTest(comic.data)
-    })
-    .catch(err => console.log(err))
-  }
-//   let comicData = document.createElement('div')
-//   comicData.classList = 'card horizontal rounded-corners'
-//   comicData.innerHTML = `
-//       <div class="card-image">
-//         <img class="materialboxed" src="https://comicvine.gamespot.com/a/uploads/scale_large/6/67663/6686014-07.jpg">
-//       </div>
-//       <div class="card-stacked">
-//         <div class="card-content">
-//           <h2>Batman #7</h2>
-//           <h3>Young Thor's Lament</h3>
-//           <h4>Cover Date: 2019-01-01</h4>
-//           <h4>Average Rating: X/10</h4>
-//           <h5>Description</h5>
-//           <p><em>A TALE OF YOUNG THOR FROM THE VIKING AGE!</em></p>
-//               <p><em>Every time Thor comes to Midgard, he gets all the mead, battle and romance a young god could possibly want. But he still can't figure out how to prove himself worthy of Mjolnir. And now Odin is determined to keep his son away from Midgard for good. And young Loki knows just the way to do it.</em></p>
-//         </div>
-//         <div class="card-action">
-//         <p class="right">Add to your collection</p>
-//         <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">+</i></a>
-//       </div>
-//     </div>
-//     `
-//   document.getElementById('comicCard').append(comicData)
-// }
-
-renderComicTest2()
